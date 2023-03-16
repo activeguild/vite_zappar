5ea1e4e90241d06ef38740b3613fb94095d08cb9
j1ngzoue <workdrive0306@gmail.com>
2023-03-16 14:42:42 +0900

    fix: worker

--- src/App.tsx
+++ src/App.tsx
@@ -12,14 +12,14 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <ZapparCanvas>
-      <ZapparCamera />
-      <mesh position={[0, 0, -5]}>
-        <sphereBufferGeometry />
-        <meshStandardMaterial color="hotpink" />
-      </mesh>
-      <directionalLight position={[2.5, 8, 5]} intensity={1.5} />
-    </ZapparCanvas>
+      <ZapparCanvas>
+        <ZapparCamera />
+        <mesh position={[0, 0, -5]}>
+          <sphereBufferGeometry />
+          <meshStandardMaterial color="hotpink" />
+        </mesh>
+        <directionalLight position={[2.5, 8, 5]} intensity={1.5} />
+      </ZapparCanvas>
   );
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import mkcert from "vite-plugin-mkcert";
+import workerPlugin from "@chialab/esbuild-plugin-worker";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     https: true,
   },
   plugins: [react(), mkcert()],
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [workerPlugin()],
+    },
+  },
 });
